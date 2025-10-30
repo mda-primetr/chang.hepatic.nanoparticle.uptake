9aa6ee2f581a682287e03229ac100cdfec0191e0
zrneale <zrneale@gmail.com>
2025-10-30 13:14:26 -0500

    Create composition plot

---
+++ microbiome_analyses/16Sv4/scripts/composition_plots.R
@@ -0,0 +1,34 @@
+# Load data as phyloseq object
+library(dplyr)
+library(phyloseq)
+library(microViz)
+physeq <- readRDS("data/physeq_rarefied.RDS") %>%
+  microViz::ps_mutate(treat = factor(treat, 
+                           levels = c("Untreated", 
+                                      "Colistin", 
+                                      "Gentamycin", 
+                                      "Kanamycin", 
+                                      "Metronidazole", 
+                                      "Vancomycin", 
+                                      "All"))
+  )
+
+# Create composition plot
+library(ggplot2)
+library(stringr)
+top_taxa_plot <- physeq%>%
+  comp_barplot(tax_level = "Genus",
+               taxon_renamer = function(x) str_replace_all(x, c( "_" = " ")),
+               sample_order = "asis",
+               n_taxa = 20,
+               label = "source_id") +
+  coord_flip() +
+  ggtitle("Top taxa") +
+  guides(fill = guide_legend(ncol = 1)) +
+  xlab("Mouse") +
+  facet_wrap(~treat, ncol = 1, scales = "free_y")
+
+
+
+
+
